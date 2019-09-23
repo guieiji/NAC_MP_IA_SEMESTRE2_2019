cb7a15e6783dd059eea228457c898da41d21a3d9
Eiji <logonaluno@local.com>
2019-09-23 14:20:56 -0300

    Atualizando Camera e player

--- Assets/Scripts/InimigosAI/MinotauroIA.cs
+++ Assets/Scripts/InimigosAI/MinotauroIA.cs
@@ -248,26 +248,25 @@ public class MinotauroIA : MonoBehaviour
         //Debug.Log("acelerou");
     }
 
-    //private void OnCollisionEnter(Collision col)
-    //{
-    //    Debug.Log("Colisao");
+    private void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Colisao");
 
-    //    if (col.collider.CompareTag("Parede"))
-    //    {
-    //        Debug.Log("AcertouParede");
-    //        estadoAtual = Estados.Esperar;
-    //        alvo = transform;
-    //        ResetarNavMesh();
-    //    }
+        if (col.collider.CompareTag("Player"))
+        {
+            Debug.Log("AcertouPlayer");
+            Atordoado();
 
-    //    if (col.collider.CompareTag("Obstaculo"))
-    //    {
-    //    }
+        }
 
-    //    if (col.collider.CompareTag("Player"))
-    //    {
-    //    }
-    //}
+        if (col.collider.CompareTag("Obstaculo"))
+        {
+        }
+
+        if (col.collider.CompareTag("Player"))
+        {
+        }
+    }
 
     private void ResetarNavMesh()
     {
